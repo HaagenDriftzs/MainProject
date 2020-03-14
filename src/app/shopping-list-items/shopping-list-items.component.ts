8247c98f3bb730425e70b0be71e5216ddf5a168b
HaagenDriftzs <C16394453@mydit.ie>
2020-03-14 18:02:06 +0000

    I have set up links to the pages and have an authentication page running

---
+++ src/app/shopping-list-items/shopping-list-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-items',
+  templateUrl: './shopping-list-items.component.html',
+  styleUrls: ['./shopping-list-items.component.css']
+})
+export class ShoppingListItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
