f9d3471902570ecd1aea470c8b08e3b41d4e356f
HaagenDriftzs <C16394453@mydit.ie>
2020-03-14 15:54:49 +0000

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {AuthenticationSectionComponent} from './authenticationSection/authenticationSection.component';
+import { ShoppingListItemsComponent } from './shopping-list-items/shopping-list-items.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthenticationSectionComponent,
+    ShoppingListItemsComponent,
+    ProductsComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/authenticationSection/authenticationSection.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './authenticationSection.component.html'
+})
+export class AuthenticationSectionComponent {
+
+}
